716a635b4dc5b389f162250cb71ffcc3164a8838
Tim Vanherwegen <timvanherwegen@MacBook-Pro-2.local>
2024-10-10 09:40:16 +0200

    initial commit

---
+++ db_client.py
@@ -0,0 +1,76 @@
+import os
+from openai import OpenAI
+#Create connection with vectorDB(Milvus)
+from pymilvus import MilvusClient
+from tqdm import tqdm
+import json
+
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+openai_client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+def emb_text(text):
+    return (
+        openai_client.embeddings.create(input=text, model="text-embedding-3-small")
+        .data[0]
+        .embedding
+    )
+
+def emb_text_d756(text):
+    return (
+        openai_client.embeddings.create(input=text, model="text-embedding-3-small", dimensions=756)
+        .data[0]
+        .embedding
+    )
+
+
+def get_cloud_client():
+    CLUSTER_ENDPOINT = "https://in03-6a1eca08d722e4c.serverless.gcp-us-west1.cloud.zilliz.com"
+    TOKEN = os.getenv('ZILLIS_API_KEY') 
+
+    client = MilvusClient(
+        uri=CLUSTER_ENDPOINT,
+        token=TOKEN 
+    )
+    return client
+
+def get_db_client():
+    milvus_client = MilvusClient(uri="http://localhost:19530")
+    return milvus_client
+
+def create_new_db_client(collection_name):
+    milvus_client = MilvusClient(uri="http://localhost:19530")
+
+    DIMENSION = 1536  # Dimension of vector embedding
+    if milvus_client.has_collection(collection_name):
+        milvus_client.drop_collection(collection_name)
+
+    milvus_client.create_collection(
+        collection_name=collection_name,
+        dimension=DIMENSION,
+        metric_type="COSINE",  # Inner product distance
+        consistency_level="Strong",  # Strong consistency level
+    )
+    return milvus_client
+
+def get_query_embeddings(client,question, collection_name):
+    search_res = client.search(
+        collection_name=collection_name,
+        data=[
+            emb_text_d756(question)
+        ],  # Use the `emb_text` function to convert the question to an embedding vector
+        limit=10,  # Return top 3 results
+        search_params={"metric_type": "COSINE", "params": {}},  # Inner product distance
+        output_fields=["text"],  # Return the text field
+    )
+    retrieved_lines_with_distances = [(res["entity"]["text"]) for res in search_res[0]]
+    return retrieved_lines_with_distances
+
+def insert_embeddings(client, embeddings, collection_name):
+    data = []
+
+    for i,line in enumerate(tqdm(embeddings, desc="Creating embeddings")):
+        data.append({"id": i, "vector": line[1], "text": line[0]})
+    client.insert(collection_name=collection_name, data=data)
+
---
+++ streamlit_test.py
@@ -0,0 +1,233 @@
+import streamlit as st
+import os
+import pickle
+from dotenv import load_dotenv
+from db_client import get_query_embeddings, get_cloud_client
+from openai import OpenAI
+import time
+
+
+st.set_page_config(layout="wide", page_title="Fintrax Knowledge Center", page_icon="images/FINTRAX_EMBLEM_POS@2x_TRANSPARENT.png")
+
+# Load environment variables
+load_dotenv()
+
+# Set up OpenAI client
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+COLLECTION_NAME = "openai_vectors"  # Milvus collection name
+client = OpenAI(api_key=OPENAI_API_KEY)
+milvus_client = get_cloud_client()
+
+#CSS injection that makes the user input right-aligned
+st.markdown(
+    """
+<style>
+    .st-emotion-cache-1c7y2kd {
+        flex-direction: row-reverse;
+        text-align: right;
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
+# st.markdown(
+#     """
+#     <style>
+#     div.stButton > button {
+#         text-align: left;  /* Align the text to the left */
+#         width: 100%;  /* Optional: Make the button full width */
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
+
+def create_llm_prompt(question, retrieved_chunks): 
+    # Combine the user's query and the retrieved document chunks into a single prompt
+    prompt = f"Vraag van de gebruiker: {question}\n\n"
+    prompt += "Relevante context:\n"
+    
+    for i, chunk in enumerate(retrieved_chunks):
+        prompt += f"{i+1}. {chunk}\n"
+    
+    return prompt
+
+col1, col2, col3 = st.sidebar.columns([1,6,1])
+col2.image("images/thumbnail-modified.png")
+
+
+# Display two buttons stacked vertically
+with st.sidebar.container(border=True):
+    sidecol1, sidecol2, sidecode3 = st.columns(3)
+    sidecol2.title("Acties")
+
+    chatbot_button = st.button("Knowledge Center", use_container_width=True)
+    upload_button = st.button("Upload Files", use_container_width=True)
+    connecties = st.button("Connecties", use_container_width=True)
+    voorkeuren = st.button("Voorkeuren", use_container_width=True)
+    rapporten = st.button("Rapporten", use_container_width=True)
+
+
+# Maintain the user's selection between the buttons
+if "active_section" not in st.session_state:
+    st.session_state["active_section"] = "Chatbot"
+
+# Switch to Chatbot if the button is clicked
+if chatbot_button:
+    st.session_state["active_section"] = "Chatbot"
+
+# Switch to Upload Files if the button is clicked
+if upload_button:
+    st.session_state["active_section"] = "Upload Files"
+
+if connecties:
+    st.session_state["active_section"] = "Connecties"
+
+if voorkeuren:
+    st.session_state["active_section"] = "Voorkeuren"
+
+if rapporten:
+    st.session_state["active_section"] = "Rapporten"
+
+
+# If Chatbot is selected
+if st.session_state["active_section"] == "Chatbot":
+    st.title("Knowledge Center")
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-4o"
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role":"system", "content":"""Het is jouw taak om een feitelijk antwoord op de gesteld vraag op basis van de gegeven context.
+                                                                    BEANTWOORD ENKEL DE VRAAG ALS HET EEN FINANCIELE VRAAG IS!
+                                                                    BEANTWOORD ENKEL ALS DE VRAAG RELEVANTE CONTEXT HEEFT!!
+                                                                    Als de context codes of vaken bevatten, moet de focus op de codes en vakken liggen.
+                                                                    Je antwoord MAG NIET iets zeggen als “volgens de passage” of “context”.
+                                                                    Maak je antwoord overzichtelijk met opsommingstekens indien nodig.
+                                                                    Jij bent een vertrouwd financieel expert in België die mensen helpt met perfect advies.
+                                                                    GEEF VOLDOENDE INFORMATIE!""" },
+                                        {"role": "assistant", "content": "Hallo, hoe kan ik je helpen? Stel mij al je financiële vragen!"}]
+
+    for message in st.session_state.messages:
+        if message["role"] != "system":
+            if message["role"] == "assistant": 
+                with st.chat_message(message["role"], avatar="images/thumbnail.png"):
+                    st.markdown(message["content"])
+            else:
+                with st.chat_message(message["role"]):
+                    st.markdown(message["content"])
+
+    if prompt := st.chat_input("Stel hier je vraag!"):
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.markdown(prompt)
+
+        with st.chat_message("assistant", avatar="images/thumbnail.png"):
+            mess = create_llm_prompt(prompt, get_query_embeddings(milvus_client, prompt, COLLECTION_NAME))
+            st.session_state.messages.append({"role": "system", "content": mess})
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+# If Upload Files is selected
+elif st.session_state["active_section"] == "Upload Files":
+    st.title("Upload Files")
+    
+    uploaded_file = st.file_uploader("Choose a file", type=["txt", "pdf", "docx", "csv"])
+    
+    if uploaded_file is not None:
+        # Display basic information about the uploaded file
+        st.write(f"Uploaded file: {uploaded_file.name}")
+        
+        # Example: Reading text files and displaying the content
+        if uploaded_file.type == "text/plain":
+            content = uploaded_file.read().decode("utf-8")
+            st.text_area("File Content", content, height=300)
+        
+        # Example for handling other file types (CSV, PDF, etc.) can be added here
+        file_type = uploaded_file.type
+        st.write(f"File type: {file_type}")
+
+
+# If Upload Files is selected
+elif st.session_state["active_section"] == "Connecties":
+    st.title("Connecties")
+    fin1,fin2,fin3 = st.columns(3)
+    with fin1:
+        with st.container(border=True):
+            fincol1, fincol2 = st.columns(2)
+            fincol1.image("images/silverfin-logo.png")
+            fincol2.subheader("Silverfin")
+            fincol2.markdown("""Naam: Fiduciaire ABC
+                                ID: 561
+                                mark@abcaccouting.be""")
+            verbindfin = st.button("Connecteer Silverfin", use_container_width=True)
+            if verbindfin:
+                with st.spinner("Verbinden..."):
+                    time.sleep(3)
+                st.success("Verbonden!")
+
+    with fin2:
+        with st.container(border=True):
+            fincol1, fincol2 = st.columns(2)
+            fincol1.image("images/mmf-logo.png")
+            fincol2.subheader("MyMinFin")
+            fincol2.markdown("""Naam: FIDUCIAIRE TIMMERMANS 
+                                support@fintrax.io""")
+            verbindmy = st.button("Connecteer MyMinFin", use_container_width=True)
+            if verbindmy:
+                with st.spinner("Verbinden..."):
+                    time.sleep(3)
+                st.success("Verbonden!")
+
+
+elif st.session_state["active_section"] == "Voorkeuren":
+    st.title("Voorkeuren")
+    st.markdown("Maak hier je standaardvragen of rapporten aan")
+    # Initialize session state to store questions
+    if 'questions' not in st.session_state:
+        st.session_state.questions = []
+
+    # Function to add a new question input field
+    def add_question():
+        st.session_state.questions.append("")
+    
+    def remove_question():
+        if st.session_state.questions != 0:
+            st.session_state.questions=st.session_state.questions[:-1]
+
+
+    add, rem, _ = st.columns([1,1,30])
+    # Button to add a new question dynamically
+    add.button('\+', on_click=add_question)
+    rem.button('\-', on_click=remove_question)
+
+    # Display the input fields for the questions dynamically
+    for i, question in enumerate(st.session_state.questions):
+        st.session_state.questions[i] = st.text_input(f"Question {i+1}:", value=question, key=f"question_{i}")
+
+    # Button to save the form
+    if st.button("Opslaan"):
+        st.success("Opgeslagen!")
+
+
+
+    
+
+
+elif st.session_state["active_section"] == "Rapporten":
+    st.title("Rapporten")
+    st.dataframe({"Vragen": st.session_state.questions}, width=800)
+    if st.button('Voer Uit'):
+        time.sleep(3)
+        st.success("Uitgevoerd!")
